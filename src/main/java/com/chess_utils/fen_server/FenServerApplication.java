d59a2c0848d892a41d1218a87b1663ee74a944aa
Ernesto Orozco <96429978+gmdeorozco@users.noreply.github.com>
2023-01-13 05:29:44 +0000

    first commit

---
+++ src/main/java/com/chess_utils/fen_server/FenServerApplication.java
@@ -0,0 +1,13 @@
+package com.chess_utils.fen_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenServerApplication.class, args);
+	}
+
+}
